8c46bc7b1fb5183f2641c5ca8b6d2bb2fd886af6
Kelsey <karonwang77@gmail.com>
2025-05-06 17:15:49 +0800

    Create a forms.py file in app directory

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send Message')
+
+class NewsletterForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Subscribe')
